86d6b22bb41304cdc7fc59bfcb5603c161c6e011
Bruno da Cunha Felipe <bruno.dc.felipe@gmail.com>
2024-05-29 18:36:52 -0300

    ajustes

--- app/Libraries/EmailSender.php
+++ app/Libraries/EmailSender.php
@@ -18,7 +18,26 @@ class EmailSender
 	{
 		$email = \Config\Services::email();
 
-		$config = [
+		$config = $this->getConfig();
+		
+		$email->initialize($config);
+
+		$email->setFrom($to, 'Your Name');
+		$email->setTo('bruno.dc.felipe@gmail.com');
+		// $email->setCC('another@another-example.com'); => Email da Empresa para vaga		
+
+		$template = view('template\emails\inscricao');
+
+		$email->setSubject("email teste");
+		$email->setMessage($template);	
+				
+		$email->send();
+		// $email->printDebugger();		
+	}   
+
+	private function getConfig():array
+	{
+		return [
 			'protocol' => 'smtp',
 			'SMTPHost' => 'sandbox.smtp.mailtrap.io',
 			'SMTPUser' => '5d950f417d719c',
@@ -28,21 +47,7 @@ class EmailSender
 			// 'mailType' => 'html',
 			// 'charset'  => 'utf-8',
 		];
-		
-		$email->initialize($config);
-
-		$email->setFrom($to, 'Your Name');
-		$email->setTo('bruno.dc.felipe@gmail.com');
-		// $email->setCC('another@another-example.com'); => Email da Empresa para vaga		
-
-		// $template = view('template/emails/inscricao');
-
-		$email->setSubject("email teste");
-		$email->setMessage("Email enviado");	
-				
-		$email->send();
-		// $email->printDebugger();		
-	}   
+	}
 
 	public function sendEmail()
 	{	
